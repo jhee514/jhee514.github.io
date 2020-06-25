2fcfdca3d13e87f2b857867330caa25ed3c2f40c
Kim, Ji Hee <514kim@gmail.com>
2020-06-26 01:40:47 +0900

    add vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+
+
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+const routes = [
+  // { path: '/foo', component: Foo },
+
+]
+
+const router = new VueRouter({
+  routes // `routes: routes`의 줄임
+})
+
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
