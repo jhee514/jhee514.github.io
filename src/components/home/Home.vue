a318bd4642a0b9e82a62676b7216d3bc704b6fda
Kim, Ji Hee <514kim@gmail.com>
2020-11-03 01:41:12 +0900

    update

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -4,11 +4,31 @@
 		<full-page>
 			<div class="section">
 				<div>
-					Pinot
+					Pinot 1
 				</div>
 				<div>
 				<button @click="$refs.fullpage.api.moveSectionDown()">
 					Next
+					</button>	
+				</div>
+				</div>
+			<div class="section">
+				<div>
+					Noir 2
+				</div>
+				<div>
+				<button @click="$refs.fullpage.api.moveSectionUp()">
+					Prev
+					</button>
+				</div>	
+			</div>
+      <div class="section">
+				<div>
+					Pinot 3
+				</div>
+				<div>
+				<button @click="$refs.fullpage.api.moveSectionDown()">
+					Next 4
 					</button>	
 				</div>
 				</div>
